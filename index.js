4b31f87b322959295fc335032566dc8b744a1dc4
sostojic <sostojic00@gmail.com>
2020-11-14 15:43:10 +0100

    add loger function

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
 const htmlDir = `${__dirname}/html`;
+const logsDir = `${__dirname}/logs`;
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Logger
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+app.use((req, res, next) => {
+    const date = new Date();
+    const logText = `**${date.getHours()}:${date.getMinutes()} - "${req.url}"**`;
+  
+    try {
+      if (fs.existsSync(`${logsDir}/URL_logger.txt`)) {
+        fs.appendFile(`${logsDir}/URL_logger.txt`, `\n${logText}`, (error) => {
+          if (error) {
+            throw Error(error);
+          }
+  
+          next();
+        });
+      } else {
+        fs.writeFile(`${logsDir}/URL_logger.txt`, logText, (error) => {
+          if (error) {
+            throw Error(error);
+          }
+  
+          next();
+        });
+      }
+    } catch (error) {
+      throw Error(error);
+    }
+  });
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Root route
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -19,5 +51,6 @@ app.get("/about-us", (req, res) => res.sendFile(`${htmlDir}/about-us.html`));
 app.get("*", (req, res) => res.sendFile(`${htmlDir}/404.html`));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
+
